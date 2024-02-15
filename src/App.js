7194cf4a18c8bf5c5dc52ba3c3b31aee82a45c76
Harsha979 <dharshavardhanr@gmail.com>
2024-02-15 20:07:31 +0530

    added h3 header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
         <h1>welcome to first project deployment</h1>
         <h2>deployed sucessfully</h2>
+        <h3>commands <br/>npm install gh-pages
+        <br/>
+        git status<br/>
+        git add .<br/>
+        git commit -m"message"<br/>
+        git push<br/>
+        npm run deploy
+        </h3>
     </div>
   );
 }
